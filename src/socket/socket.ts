4731080055116915945305b355edef5c4fb290c4
sujith <syjithkpy@gmail.com>
2025-04-02 14:41:31 +0530

    socket connection issue 1

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  path: "/socket.io/", 
   transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 10,
